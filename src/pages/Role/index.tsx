22ab6a9564427ffa42389411c088b580b7571ce9
iNeelPatel <neelpatel.6531@gmail.com>
2020-09-03 00:00:20 +0530

    add page layout

--- src/pages/Role/index.tsx
+++ src/pages/Role/index.tsx
@@ -7,6 +7,7 @@ import Textfield from "@atlaskit/textfield";
 import Form, { Field } from "@atlaskit/form";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import Page, { Grid, GridColumn } from "@atlaskit/page";
 
 // ====================================== File imports ======================================
 import { Props, AddRoleForm } from "./types";
@@ -62,97 +63,109 @@ const Role = (props: Props) => {
    }, [props.access]);
 
    return (
-      <div style={{ display: "flex", flex: 1, flexDirection: "column" }}>
-         <Breadcrumb
-            items={breadcrumbItems}
-            screen="Role"
-            right={
-               <div style={{ display: "flex" }}>
-                  <Form
-                     onSubmit={async (addRoleForm: AddRoleForm) => {
-                        try {
-                           await props.createRole(addRoleForm);
-                        } catch (error) {}
-                     }}
-                  >
-                     {({ formProps, submitting }: any) => (
-                        <form {...formProps} style={{ display: "flex" }}>
-                           <Field label="" isRequired name="roleName" defaultValue="">
-                              {({ fieldProps }: any) => <Textfield {...fieldProps} placeholder="Role Name" style={{ width: 200 }} />}
-                           </Field>
-                           <Button
-                              type="submit"
-                              style={{ height: 38, marginLeft: 10, marginTop: 9 }}
-                              appearance="primary"
-                              isLoading={submitting}
-                           >
-                              Add new role
-                           </Button>
-                        </form>
-                     )}
-                  </Form>
-               </div>
-            }
-         />
-         {loading ? (
-            <ScreenLoader />
-         ) : (
-            <div style={{ display: "flex" }}>
-               <div>
-                  {roleItems.map((item, idx) => (
-                     <div
-                        className="cell"
-                        style={{ borderColor: colors.N60, fontWeight: "bold", minWidth: 210, justifyContent: "left" }}
-                        key={item + idx}
-                     >
-                        {item}
+      <Page>
+         <Grid spacing="compact" layout="fluid">
+            <GridColumn medium={12}>
+               <Breadcrumb
+                  items={breadcrumbItems}
+                  screen="Role"
+                  right={
+                     <div style={{ display: "flex" }}>
+                        <Form
+                           onSubmit={async (addRoleForm: AddRoleForm) => {
+                              try {
+                                 await props.createRole(addRoleForm);
+                              } catch (error) {}
+                           }}
+                        >
+                           {({ formProps, submitting }: any) => (
+                              <form {...formProps} style={{ display: "flex" }}>
+                                 <Field label="" isRequired name="roleName" defaultValue="">
+                                    {({ fieldProps }: any) => <Textfield {...fieldProps} placeholder="Role Name" style={{ width: 200 }} />}
+                                 </Field>
+                                 <Button
+                                    type="submit"
+                                    style={{ height: 38, marginLeft: 10, marginTop: 9 }}
+                                    appearance="primary"
+                                    isLoading={submitting}
+                                 >
+                                    Add new role
+                                 </Button>
+                              </form>
+                           )}
+                        </Form>
                      </div>
-                  ))}
-               </div>
-               <div style={{ display: "flex", flex: 1, overflow: "scroll", background: colors.N10, maxWidth: 750 }}>
-                  {Object.keys(roles).map((role: string, idx: number) => (
-                     <div key={role + idx} style={{ display: "flex", flex: 1, flexDirection: "column" }}>
-                        <div className="cell" style={{ borderColor: colors.N60, fontWeight: "bold", background: colors.N0 }}>
-                           {role}
+                  }
+               />
+            </GridColumn>
+            {loading ? (
+               <ScreenLoader />
+            ) : (
+               <React.Fragment>
+                  <GridColumn medium={3}>
+                     {roleItems.map((item, idx) => (
+                        <div
+                           className="cell"
+                           style={{
+                              borderColor: colors.N60,
+                              fontWeight: "bold",
+                              minWidth: 210,
+                              justifyContent: "left",
+                              overflow: "scroll",
+                           }}
+                           key={item + idx}
+                        >
+                           {item}
                         </div>
-                        {Object.keys(roles[role]).map((roleItems: any, idx: number) => (
-                           <div className="cell" style={{ borderColor: colors.N60 }} key={role + roleItems + idx}>
-                              <Checkbox
-                                 value="R"
-                                 label="R"
-                                 isDisabled={role === "admin" || roleItems === "role"}
-                                 defaultChecked={roles[role][roleItems].read}
-                                 onChange={async () => {
-                                    var updateRoles = roles;
-                                    updateRoles[role][roleItems]["read"] = !roles[role][roleItems].read;
-                                    await props.updateRole({ name: role, permission: updateRoles[role] });
-                                    setRoles(updateRoles);
-                                 }}
-                                 name={`checkbox-r-${role + roleItems + idx}`}
-                                 testId={`cb-default-r-${role + roleItems + idx}`}
-                              />
-                              <Checkbox
-                                 value="W"
-                                 label="W"
-                                 isDisabled={role === "admin" || roleItems === "role"}
-                                 defaultChecked={roles[role][roleItems].write}
-                                 onChange={async () => {
-                                    var updateRoles = roles;
-                                    updateRoles[role][roleItems]["write"] = !roles[role][roleItems].write;
-                                    await props.updateRole({ name: role, permission: updateRoles[role] });
-                                    setRoles(updateRoles);
-                                 }}
-                                 name={`checkbox-w-${role + roleItems}`}
-                                 testId={`cb-default-w-${role + roleItems}`}
-                              />
+                     ))}
+                  </GridColumn>
+                  <GridColumn medium={9}>
+                     <div style={{ display: "flex", flex: 1, overflow: "scroll", background: colors.N10 }}>
+                        {Object.keys(roles).map((role: string, idx: number) => (
+                           <div key={role + idx} style={{ display: "flex", flex: 1, flexDirection: "column" }}>
+                              <div className="cell" style={{ borderColor: colors.N60, fontWeight: "bold", background: colors.N0 }}>
+                                 {role}
+                              </div>
+                              {Object.keys(roles[role]).map((roleItems: any, idx: number) => (
+                                 <div className="cell" style={{ borderColor: colors.N60 }} key={role + roleItems + idx}>
+                                    <Checkbox
+                                       value="R"
+                                       label="R"
+                                       isDisabled={role === "admin" || roleItems === "role"}
+                                       defaultChecked={roles[role][roleItems].read}
+                                       onChange={async () => {
+                                          var updateRoles = roles;
+                                          updateRoles[role][roleItems]["read"] = !roles[role][roleItems].read;
+                                          await props.updateRole({ name: role, permission: updateRoles[role] });
+                                          setRoles(updateRoles);
+                                       }}
+                                       name={`checkbox-r-${role + roleItems + idx}`}
+                                       testId={`cb-default-r-${role + roleItems + idx}`}
+                                    />
+                                    <Checkbox
+                                       value="W"
+                                       label="W"
+                                       isDisabled={role === "admin" || roleItems === "role"}
+                                       defaultChecked={roles[role][roleItems].write}
+                                       onChange={async () => {
+                                          var updateRoles = roles;
+                                          updateRoles[role][roleItems]["write"] = !roles[role][roleItems].write;
+                                          await props.updateRole({ name: role, permission: updateRoles[role] });
+                                          setRoles(updateRoles);
+                                       }}
+                                       name={`checkbox-w-${role + roleItems}`}
+                                       testId={`cb-default-w-${role + roleItems}`}
+                                    />
+                                 </div>
+                              ))}
                            </div>
                         ))}
                      </div>
-                  ))}
-               </div>
-            </div>
-         )}
-      </div>
+                  </GridColumn>
+               </React.Fragment>
+            )}
+         </Grid>
+      </Page>
    );
 };
 
