669ec46685fa5e0137a9b3a08014b7a3e0affb63
iNeelPatel <neelpatel.6531@gmail.com>
2020-09-24 00:07:25 +0530

    add parameter form

--- src/pages/Parameter/AddParameter/AddParameterForm.tsx
+++ src/pages/Parameter/AddParameter/AddParameterForm.tsx
@@ -2,6 +2,9 @@
 import React from "react";
 import Page, { Grid, GridColumn } from "@atlaskit/page";
 import AppState from "../../../redux/types";
+import Form, { Field, ErrorMessage } from "@atlaskit/form";
+import Button from "@atlaskit/button";
+import Textfield from "@atlaskit/textfield";
 import { connect } from "react-redux";
 
 // ====================================== File imports ======================================
@@ -11,7 +14,52 @@ const AddSampleGroup = (props: AddParameterFormProps) => {
    return (
       <Page>
          <Grid spacing="compact" layout="fluid">
-            <GridColumn>Add Parameter from</GridColumn>
+            <GridColumn medium={12}>
+               <Form
+                  onSubmit={async (data: any) => {
+                     console.log(data);
+
+                     try {
+                        await props.onSubmit(data);
+                        props.onBack();
+                     } catch (err) {
+                        console.log(err);
+                     }
+                  }}
+               >
+                  {({ formProps, submitting }: any) => (
+                     <form {...formProps}>
+                        <Grid>
+                           <GridColumn medium={8}>
+                              <Field label="Name" isRequired name="name">
+                                 {({ fieldProps }: any) => <Textfield {...fieldProps} />}
+                              </Field>
+                           </GridColumn>
+                           <GridColumn medium={4}>
+                              <Field label="Unit" isRequired name="unit">
+                                 {({ fieldProps }: any) => <Textfield {...fieldProps} />}
+                              </Field>
+                           </GridColumn>
+                        </Grid>
+                        <Field label="Method" isRequired name="method">
+                           {({ fieldProps }: any) => <Textfield {...fieldProps} />}
+                        </Field>
+                        <Field label="Department" isRequired name="department">
+                           {({ fieldProps }: any) => <Textfield {...fieldProps} />}
+                        </Field>
+
+                        <div style={{ display: "flex", justifyContent: "flex-end", marginTop: 20 }}>
+                           <Button appearance="link" disabled={submitting} onClick={() => props.onBack()}>
+                              Back
+                           </Button>
+                           <Button type="submit" appearance="primary" isLoading={submitting}>
+                              Add parameter
+                           </Button>
+                        </div>
+                     </form>
+                  )}
+               </Form>
+            </GridColumn>
          </Grid>
       </Page>
    );
